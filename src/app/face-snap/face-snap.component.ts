b776627bdb24751d6c8e85219932ddfe268f8d12
Ben <lavaud.benoit@hotmail.com>
2022-03-22 16:13:51 +0100

    New component FaceSnap

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-face-snap',
   templateUrl: './face-snap.component.html',
   styleUrls: ['./face-snap.component.scss']
 })
-export class FaceSnapComponent {
+export class FaceSnapComponent implements OnInit {
+
+  title!: string;
+  description!: string;
+  createdDate!: Date;
+  snaps!: number;
+  ImgUrl!: string;
+
+  ngOnInit(): void {
+    this.title = 'Arthur';
+    this.description = 'My friend';
+    this.createdDate = new Date();
+    this.snaps = 6;
+    this.ImgUrl = 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg';
+  }
+
+  onAddSnap(){
+    this.snaps++;
+  }
 }
