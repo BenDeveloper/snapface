06aa98d2edf2784380ecc67d0feab8f13e201a42
Ben <lavaud.benoit@gmail.com>
2022-03-31 16:17:09 +0200

    LocaleData, date pipe.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FaceSnapComponent } from './face-snap/face-snap.component';
+import { registerLocaleData } from '@angular/common';
+import * as en from '@angular/common/locales/en';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,13 @@ import { FaceSnapComponent } from './face-snap/face-snap.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'en-GB'}
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor() {
+    registerLocaleData(en.default);
+  }
+}
