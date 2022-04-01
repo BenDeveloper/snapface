1d71ff40e64fc038f0484115d1bfd878bdd5fa0b
Ben <lavaud.benoit@gmail.com>
2022-04-01 13:29:45 +0200

    FaceSnapsService

---
+++ src/app/services/face-snaps.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { FaceSnap } from '../models/face-snap.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class FaceSnapsService {
+    faceSnaps : FaceSnap[] = [
+      { 
+        title: 'Arthur', 
+        description: 'king of Kamelott', 
+        imageUrl: 'https://www.serieously.com/app/uploads/2021/08/sans-titre-25-20-1024x572.jpg', 
+        snaps: 10, 
+        createdDate: new Date,
+        location: 'Kaamelott'
+      },
+      { 
+        title: 'Karadoc', 
+        description: 'Fat is life', 
+        imageUrl: 'https://c.tenor.com/f_cJgAsY-j4AAAAC/kaamelott-karadoc.gif', 
+        snaps: 100, 
+        createdDate: new Date 
+      },
+      { 
+        title: 'Perceval', 
+        description: 'Knight', 
+        imageUrl: 'https://fr.web.img3.acsta.net/newsv7/16/11/25/16/38/164818.jpg', 
+        snaps: 90, 
+        createdDate: new Date 
+      }];
+  
+}
